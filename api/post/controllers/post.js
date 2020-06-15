224e7204fa024a6077c89f5d5e7ac905e7ebf401
Gallo Da Sballo <alessandro@hubmade.com>
2020-06-15 17:53:46 +0200

    CRUD5 Extend custom controller to set Likes

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,22 @@
 'use strict';
 
+const { parseMultipartData, sanitizeEntity} = require('strapi-utils')
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+        let entity
+        if (ctx.is('multipart')){
+            const {data, files} = parseMultipartData(ctx)
+            entity = await strapi.services.post.create({...data, likes: 0}, {files})
+        } else {
+            entity = await strapi.services.post.create({...ctx.request.body, likes: 0})
+        }
+
+        return sanitizeEntity(entity, {model: strapi.models.post})
+    }
+};
