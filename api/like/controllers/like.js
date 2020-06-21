50fe129e0e6023eb917946be3e2493aa3a52df1e
Gallo Da Sballo <alessandro@hubmade.com>
2020-06-21 19:13:04 +0200

    33 - Likes 2 - Set up custom Create Controller for Like Creation

--- api/like/controllers/like.js
+++ api/like/controllers/like.js
@@ -1,8 +1,50 @@
 'use strict';
-
+const {parseMultipartData, sanitizeEntity} = require('strapi-utils')
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx){
+        let entity
+
+        const {user} = ctx.state //Logged in User
+        const {post} = ctx.request.body //Id of Post
+
+        if(typeof post !== 'number'){
+            ctx.throw(400, "Please only pass the id for the post")
+        }
+
+        const realPost = await strapi.services.post.findOne({
+            id: post
+        })
+        if(!realPost){
+            ctx.throw(400, "This post doesn't exist")
+        }
+
+        const found = await strapi.services.like.findOne({
+            user: user.id,
+            post
+        })
+        if(found){
+            ctx.throw(400, "You already liked this post")
+        }
+
+        if(ctx.is('multipart')){
+            ctx.throw(400, "Only make JSON requests")
+        } else {
+            entity = await strapi.services.like.create({post, user})
+        }
+
+        //Update the likes counter
+        const {likes} = realPost
+        const updatedPost = await strapi.services.post.update({
+            id: post
+        }, {
+            likes: likes + 1
+        })
+
+        return sanitizeEntity(entity, {model: strapi.models.like})
+    }
+};
