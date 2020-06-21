c2a7ef1c913eb5190c4d2649a378ed257ece09a1
Gallo Da Sballo <alessandro@hubmade.com>
2020-06-21 13:55:58 +0200

    30 - Auth 4 - Show Edit and Delete buttons only if logged in AND you own the post

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -29,5 +29,30 @@ module.exports = {
         }
 
         return sanitizeEntity(entity, {model: strapi.models.post})
+    },
+
+    async update(ctx){
+        const {id} = ctx.params
+        const {user} = ctx.state
+
+        let entity
+        if (ctx.is('multipart')){
+            ctx.throw(400, "Please only make JSON request with an updated description")
+        } else {
+            delete ctx.request.body.likes
+
+            entity = await strapi.services.post.update({id, author: user.id}, ctx.request.body)
+        }
+
+        return sanitizeEntity(entity, {model: strapi.models.post})
+    },
+
+    async delete(ctx){
+        const {id} = ctx.params
+        const {user} = ctx.state
+
+        const entity = await strapi.services.post.delete({id, author: user.id})
+
+        return sanitizeEntity(entity, {model: strapi.models.post})
     }
 };
