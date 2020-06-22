9b7d422434df87b157b28b2958d81d87f0d13793
Gallo Da Sballo <alessandro@hubmade.com>
2020-06-22 22:10:02 +0200

    42 - CODE FINAL - Final Video in which we set up a cron job and send email with the count of likes

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -18,4 +18,27 @@ module.exports = {
   // '0 1 * * 1': () => {
   //
   // }
+
+  '0 0 0 * * *': async () => {
+    console.log("Once a day, at midnight")
+
+    //Retrieve the users
+    const users = await strapi.plugins['users-permissions'].services.user.fetchAll({})
+
+    const res = await Promise.all(users.map(async user => {
+      //Retrieve all posts created by this user
+      const posts = await strapi.services.post.find({author: user.id})
+
+      console.log("posts.length", posts.length)
+
+      const total = posts.reduce((acc, post) => acc + post.likes, 0)
+
+      await strapi.plugins['email'].services.email.send({
+        to: user.email,
+        from: 'Strapitest@localhost.com',
+        subject: 'Your likes total',
+        text: `You got ${total} likes!`
+      })
+    }))
+  }
 };
